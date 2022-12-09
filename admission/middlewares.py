285879b3844653f9885f5e23bd299a86425413e5
Mikhail <joinmma@gmail.com>
2022-12-09 15:05:44 +0200

    add restriction on media file receiving

---
+++ admission/middlewares.py
@@ -0,0 +1,18 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.http.response import HttpResponseForbidden
+from django.conf import settings
+
+
+class MediaAccessMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request: WSGIRequest):
+        if request.path.startswith(settings.MEDIA_URL):
+            if not (request.user.is_staff and request.user.is_superuser):
+                return HttpResponseForbidden(b'No Access!')
+
+        response = self.get_response(request)
+
+        return response
