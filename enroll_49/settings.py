d8076a48064cda72f9140181d752f6190c772f9e
Mikhail <joinmma@gmail.com>
2023-06-22 16:55:17 +0300

    comment static_root

--- enroll_49/settings.py
+++ enroll_49/settings.py
@@ -153,7 +153,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
